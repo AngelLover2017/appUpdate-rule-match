60ba39f07d90c674f296f6c9508b381d4ce3080a
master <954988021@qq.com>
2021-11-10 20:03:12 +0800

    增加对规则可用性的判断； 修改Rule，不包含白名单字段以及其他用不上的字段，提高查询效率，白名单完全交给缓存判断； 删除对缓存的更新，缓存的更新交给修改配置端； 增加对下载量的统计

--- src/main/java/com/example/demo/mapper/RuleMapper.java
+++ src/main/java/com/example/demo/mapper/RuleMapper.java
@@ -9,6 +9,11 @@ import org.springframework.transaction.annotation.Transactional;
 import java.util.List;
 @Mapper
 public interface RuleMapper {
-    @Select("select * from rule where aid = #{aid} and platform = #{device_platform} and cpu_arch= #{cpu_arch} and channel=#{channel}")
+    @Select("select id,download_url,update_version_code,update_version_code,md5,"
+            +"max_update_version_code,min_update_version_code,max_os_api,min_os_api,title,update_tips,download_times "
+            +"from rule "
+            +"where aid = #{aid} and platform = #{device_platform} and cpu_arch= #{cpu_arch} and channel=#{channel} and status=0")
     List<Rule> findCertainRules(Integer aid, String device_platform, String cpu_arch,String channel);
+    @Update("update rule set download_times=#{download_times} where id=#{id}")
+    void updateDownloadTimes(Integer id , Integer download_times);
 }
